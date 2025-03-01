39b487cee3050b5e261525c0db6df2fc8ef50913
jakebark <jacob.barker@gmail.com>
2025-03-01 17:34:34 +0000

    helper.go

--- internal/terraform/helpers.go
+++ internal/terraform/helpers.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
+// extractTraversalString converts a hcl hierachical/traversal string to a literal string.
 func extractTraversalString(expr hcl.Expression, caseInsensitive bool) string {
 	ste, ok := expr.(*hclsyntax.ScopeTraversalExpr)
 	if !ok {
@@ -15,14 +16,6 @@ func extractTraversalString(expr hcl.Expression, caseInsensitive bool) string {
 	}
 	tokens := []string{}
 
-	// converts hcl qualified name into a literal string
-	// eg local.tags to ... local.tags
-	// hcl sees it as hierachical components, eg:
-	// TraversalExpr{
-	// Traversal: [
-	//    TraverseRoot{Name: "local"},
-	//    TraverseAttr{Name: "tags"}
-	// ]}
 	for _, step := range ste.Traversal {
 		switch t := step.(type) {
 		case hcl.TraverseRoot:
@@ -38,6 +31,7 @@ func extractTraversalString(expr hcl.Expression, caseInsensitive bool) string {
 	return result
 }
 
+// mergeTags combines multiple tag maps
 func mergeTags(tagMaps ...map[string]bool) map[string]bool {
 	merged := make(map[string]bool)
 	for _, m := range tagMaps {
@@ -48,6 +42,7 @@ func mergeTags(tagMaps ...map[string]bool) map[string]bool {
 	return merged
 }
 
+// extractTags handles errors in extracting tags from hcl
 func extractTags(attr *hclsyntax.Attribute, caseInsensitive bool) map[string]bool {
 	tags, err := extractTagMap(attr, caseInsensitive)
 	if err != nil {
@@ -57,15 +52,15 @@ func extractTags(attr *hclsyntax.Attribute, caseInsensitive bool) map[string]boo
 	return tags
 }
 
+// extractTagMap extracts the hcl tag map to a go map
 func extractTagMap(attr *hclsyntax.Attribute, caseInsensitive bool) (map[string]bool, error) {
 	val, diags := attr.Expr.Value(nil)
-	// check for errors in tags
+
 	if diags.HasErrors() || !val.Type().IsObjectType() {
 		return nil, fmt.Errorf("failed to extract tag map")
 	}
 
 	tags := make(map[string]bool)
-	// convert hcl to go map
 	for key := range val.AsValueMap() {
 		if caseInsensitive {
 			key = strings.ToLower(key)
