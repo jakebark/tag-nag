792ebff010c76672822689ec4667123106c259ed
jakebark <jacob.barker@gmail.com>
2025-04-15 11:06:10 +0100

    rm violation from tf types

--- internal/terraform/types.go
+++ internal/terraform/types.go
@@ -7,13 +7,5 @@ type DefaultTags struct {
 	ReferencedTags TagReferences
 }
 
-type Violation struct {
-	resourceType string
-	resourceName string
-	line         int
-	missingTags  []string
-	skip         bool
-}
-
 // TagReferences maps a reference identifier to a tag map ("local.tags")
 type TagReferences map[string]shared.TagMap
