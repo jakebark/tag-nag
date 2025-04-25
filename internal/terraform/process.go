5c7807cb33fb15aa080f1d918a8b34ea1242fb5c
jakebark <jacob.barker@gmail.com>
2025-04-25 16:10:28 +0100

    amend processDir to use SkippedDirs x2

--- internal/terraform/process.go
+++ internal/terraform/process.go
@@ -65,8 +65,13 @@ func ProcessDirectory(dirPath string, requiredTags map[string][]string, caseInse
 		if err != nil {
 			return err
 		}
-		if info.IsDir() && (info.Name() == ".terraform" || info.Name() == ".git") {
-			return filepath.SkipDir
+		if info.IsDir() {
+			dirName := info.Name()
+			for _, skipped := range config.SkippedDirs {
+				if dirName == skipped {
+					return filepath.SkipDir
+				}
+			}
 		}
 		if !info.IsDir() && filepath.Ext(path) == ".tf" {
 			violations := processFile(path, requiredTags, &defaultTags, tfCtx, caseInsensitive)
