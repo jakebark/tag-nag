9cb2546e094a66be74093c2ea8b760ed3ec51a5b
jakebark <jacob.barker@gmail.com>
2025-04-27 21:27:33 +0100

    unit test for mergeTags

--- internal/terraform/helpers_test.go
+++ internal/terraform/helpers_test.go
@@ -4,10 +4,9 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/jakebark/tag-nag/internal/shared"
-	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/json"
 )
 
 func testTraversalToString(t *testing.T) {
@@ -19,19 +18,19 @@ func testTraversalToString(t *testing.T) {
 	}{
 		{
 			name:            "literal",
-			hclInput:        `Owner`,
+			hclInput:        "Owner",
 			caseInsensitive: false,
 			expected:        "Owner",
 		},
 		{
 			name:            "literal, case insensitive",
-			hclInput:        `Owner`,
+			hclInput:        "Owner",
 			caseInsensitive: true,
 			expected:        "owner",
 		},
 		{
 			name:            "traversal",
-			hclInput:        `local.network.subnets[0].id`,
+			hclInput:        "local.network.subnets[0].id",
 			caseInsensitive: false,
 			expected:        "local.network.subnets",
 		},
@@ -48,6 +47,65 @@ func testTraversalToString(t *testing.T) {
 			actual := traversalToString(expr, tc.caseInsensitive)
 			if actual != tc.expected {
 				t.Errorf("traversalToString(%q, %v) = %q; want %q", tc.hclInput, tc.caseInsensitive, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func testMergeTags(t *testing.T) {
+	testCases := []struct {
+		name     string
+		inputs   []shared.TagMap
+		expected shared.TagMap
+	}{
+		{
+			name:     "empty",
+			inputs:   []shared.TagMap{},
+			expected: shared.TagMap{},
+		},
+		{
+			name: "key",
+			inputs: []shared.TagMap{
+				{"Environment": {}},
+			},
+			expected: shared.TagMap{"Environment": {}},
+		},
+		{
+			name:     "key and value",
+			inputs:   []shared.TagMap{{"Environment": {"Dev"}}},
+			expected: shared.TagMap{"Environment": {"Dev"}},
+		},
+		{
+			name: "multiple keys and values",
+			inputs: []shared.TagMap{
+				{"Environment": {"Dev"}},
+				{"Owner": {"Prod"}},
+			},
+			expected: shared.TagMap{"Environment": {"Dev"}, "Owner": {"Prod"}},
+		},
+		{
+			name: "overlapping values, last wins",
+			inputs: []shared.TagMap{
+				{"Environment": {"Dev"}, "Owner": {"jakebark"}},
+				{"Owner": {"Jake"}, "CostCenter": {"C-01"}},
+			},
+			expected: shared.TagMap{"Environment": {"Dev"}, "Owner": {"Jake"}, "CostCenter": {"C-01"}},
+		},
+		{
+			name: "overlapping empty value, last wins",
+			inputs: []shared.TagMap{
+				{"Environment": {"Dev"}},
+				{"Environment": {}},
+			},
+			expected: shared.TagMap{"Environment": {}},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := mergeTags(tc.inputs...)
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("mergeTags() = %v; want %v", actual, tc.expected)
 			}
 		})
 	}
