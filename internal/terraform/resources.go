6062defc5ca4f49a6fc9b33d7060b01ba8fda5da
jakebark <jacob.barker@gmail.com>
2025-04-29 21:54:48 +0100

    added taggable skip to checkResources

--- internal/terraform/resources.go
+++ internal/terraform/resources.go
@@ -42,6 +42,10 @@ func checkResourcesForTags(body *hclsyntax.Body, requiredTags shared.TagMap, def
 			continue
 		}
 
+		if taggable != nil && !taggable[resourceType] {
+			continue
+		}
+
 		providerID := getResourceProvider(block, caseInsensitive)
 		providerEvalTags := defaultTags.LiteralTags[providerID]
 		if providerEvalTags == nil {
