c79845fa2638ba922fa92a046ec0d478480646e5
jakebark <jacob.barker@gmail.com>
2025-04-30 20:59:35 +0100

    tf scan comments

--- internal/terraform/scan.go
+++ internal/terraform/scan.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 )
 
-// CheckForTerraformFiles checks if any .tf files exist in the directory
+// scan looks for tf files
 func scan(dirPath string) (bool, error) {
 	found := false
 	targetExt := ".tf"
