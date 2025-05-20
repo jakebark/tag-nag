d3723251f71cf726d72b6a2a031524418c651abf
jakebark <jacob.barker@gmail.com>
2025-05-20 21:26:38 +0100

    initial cfn tests

--- main_test.go
+++ main_test.go
@@ -144,3 +144,64 @@ func TestTerraformCLI(t *testing.T) {
 		})
 	}
 }
+
+func TestCloudFormationCLI(t *testing.T) {
+	testCases := []testCases{
+		{
+			name:             "yaml",
+			filePathOrDir:    "testdata/cloudformation/single_resource.yml",
+			cliArgs:          []string{"--tags", "Owner,Environment"},
+			expectedExitCode: 0,
+			expectedError:    false,
+			expectedOutput:   []string{"No tag violations found"},
+		},
+		{
+			name:             "json",
+			filePathOrDir:    "testdata/cloudformation/single_resource.json",
+			cliArgs:          []string{"--tags", "Owner,Environment"},
+			expectedExitCode: 0,
+			expectedError:    false,
+			expectedOutput:   []string{"No tag violations found"},
+		},
+		{
+			name:             "yaml missing tags",
+			filePathOrDir:    "testdata/cloudformation/single_resource.yml",
+			cliArgs:          []string{"--tags", "Owner,Environment,Project"},
+			expectedExitCode: 1,
+			expectedError:    true,
+			expectedOutput:   []string{`AWS::S3::Bucket "this"`, "Missing tags: Project"},
+		},
+		{
+			name:             "json missing tags",
+			filePathOrDir:    "testdata/cloudformation/single_resource.json",
+			cliArgs:          []string{"--tags", "Owner,Environment,Project"},
+			expectedExitCode: 1,
+			expectedError:    true,
+			expectedOutput:   []string{`AWS::S3::Bucket "this"`, "Missing tags: Project"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			argsForRun := append([]string{tc.filePathOrDir}, tc.cliArgs...)
+			output, err, exitCode := runTagNag(t, argsForRun...)
+
+			if tc.expectedError && err == nil {
+				t.Errorf("Expected an error from command execution, but got none. Output:\n%s", output)
+			}
+			if !tc.expectedError && err != nil {
+				t.Errorf("Expected no error from command execution, but got: %v. Output:\n%s", err, output)
+			}
+
+			if exitCode != tc.expectedExitCode {
+				t.Errorf("Expected exit code %d, got %d. Output:\n%s", tc.expectedExitCode, exitCode, output)
+			}
+
+			for _, expectedStr := range tc.expectedOutput {
+				if !strings.Contains(output, expectedStr) {
+					t.Errorf("Output missing expected string '%s'. Output:\n%s", expectedStr, output)
+				}
+			}
+		})
+	}
+}
