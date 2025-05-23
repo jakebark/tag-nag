5b5b7baefa44945f42d1987b2063d59a37766e37
jakebark <jacob.barker@gmail.com>
2025-05-23 17:02:17 +0100

    rm TestParseTagsFatal

--- internal/inputs/inputs_test.go
+++ internal/inputs/inputs_test.go
@@ -143,17 +143,3 @@ func TestSplitTags(t *testing.T) {
 		})
 	}
 }
-
-func TestParseTagsFatal(t *testing.T) {
-	if os.Getenv("BE_TEST_FATAL") == "1" {
-		parseTags("Invalid[Tag")
-		return
-	}
-	cmd := exec.Command(os.Args[0], "-test.run=^TestParseTagsFatal$")
-	cmd.Env = append(os.Environ(), "BE_TEST_FATAL=1")
-	err := cmd.Run()
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		return
-	}
-	t.Fatalf("process ran with err %v, want exit status 1", err)
-}
